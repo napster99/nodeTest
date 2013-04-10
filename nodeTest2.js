5b575deedf29e53bf8e2d02906defb7cea7d3dc0
napster99 <morrisonleisure@163.com>
2013-04-10 22:22:43 +0800

    Number of times the cumulative access webpage in the file

---
+++ nodeTest2.js
@@ -0,0 +1,19 @@
+/**
+ * Number of times the cumulative access webpage in the file
+ * @author morrisonleisure@163.com Napster
+ */
+ var http = require('http')
+ 	,fs   = require('fs');
+
+
+ http.createServer(function(req, res){
+	fs.readFile('count.txt','utf-8',function(error,data){
+		res.writeHead(200,{'Content-Type':'text/plain'});
+		data = parseInt(data) + 1;
+		fs.writeFile('count.txt',data);
+		res.end('This page was refreshed '+ data + ' times!');
+	});
+ }).listen(8124);
+
+
+
