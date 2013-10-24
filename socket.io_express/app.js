c7a60aee684ddf59dde186e8f5733db2209ce313
napster99 <morrisonleisure@163.com>
2013-10-24 22:42:20 +0800

    send nickname

--- socket.io_express/app.js
+++ socket.io_express/app.js
@@ -1,19 +1,19 @@
-var express = require('express');
-
-var app = module.exports = express.createServer(),
+var app = require('express').createServer(),
 io = require('socket.io').listen(app);
 
 app.listen(3000);
 
 app.get('/',function(req,res) {
-	res.sendfile(__dirname+ '/index.html');
+	// res.sendfile(__dirname+ '/index.html');
+	res.sendfile('index.html',{root:__dirname});
 });
 
 io.sockets.on('connection',function(socket) {
-	socket.emit('welcome',{text : 'OH HAI! U R CONNECTED!'});
+	socket.on('nickname',function(data) {
+	console.log('The server received the following nickname: '+ data);
 })
 
-
+})
 
 
 
