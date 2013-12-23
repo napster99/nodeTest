b61248b0d157851c0c81a8399d72f6f9913a5bea
napster99 <morrisonleisure@163.com>
2013-12-23 16:03:37 +0800

    rank

--- node-webList/public/javascripts/siderbar.js
+++ node-webList/public/javascripts/siderbar.js
@@ -1,106 +1,148 @@
 //@charset 'utf-8';
 
 $(function() {
-	var ui = {
-		$btn : $('#signIn'),
-		$signBtn : $('#againBtn'),
-		$signStatus : $('#signStatus')
-	}
-	var uid = $('#uid').val();
-	var Page = {
-		init : function() {
-			this.view();
-			this.addEventListener();
-			this.initJsChart();
-		},
-		view : function() {
-			var signStatus = ui.$signStatus.val();
-			var curDate = (new Date()).format('yyyy-MM-dd');
-			if(signStatus == curDate) {
-				ui.$btn.text('已签到').attr('disabled','disabled');
-			}
-		},
-		addEventListener : function() {
-			var self = this;
-			$('#scratchCard')[0].seleted = false;
-			ui.$btn.on('click','',function() {
-				$('#scratchCard').show();
-			});
-			ui.$signBtn.on('click','',function() {
-				var point = (+$(this).attr('point')) + (+$('#score').text());
-				self.sendAjax(uid,point);
-			});
-		},
-		sendAjax : function(uid,score) {
-			var self = this;
-			var options = {
-				'url' : '/updateScore',
-				'dataType' : 'json',
-				'type' : 'GET',
-				'data' : {uid : uid, score : score},
-				'success' : function(data) {
-					console.log(data);
-					$('#score').text(score);
-					$('#scratchCard').hide();
-					ui.$btn.text('已签到').attr('disabled','disabled');
-					self.changeSignStatus();
-				},
-				'error' : function(err) {
-					console.log(err)
-				}
-			}
-			$.ajax(options);
-		},
-		//改变签到状态
-		changeSignStatus : function() {
-			var options = {
-				'url' : '/changeSignStatus',
-				'dataType' : 'json',
-				'type' : 'POST',
-				'success' : function(data) {
-					console.log(data)
-				},
-				'error' : function(err) {
-					console.log(err)
-				}
-			}
-			$.ajax(options);
-		},
+  var ui = {
+    $btn : $('#signIn'),
+    $signBtn : $('#againBtn'),
+    $signStatus : $('#signStatus'),
+    $rankContainer : $('#rankContainer')
+  }
+  var uid = $('#uid').val();
+  var Page = {
+    pageData : {
+      user : {},
+      userSort : {}
+    },
+    init : function() {
+      this.view();
+      this.addEventListener();
+      this.initJsChart();
+    },
+    view : function() {
+      var signStatus = ui.$signStatus.val();
+      var curDate = (new Date()).format('yyyy-MM-dd');
+      if(signStatus == curDate) {
+        ui.$btn.text('已签到').attr('disabled','disabled');
+      }
+    },
+    addEventListener : function() {
+      var self = this;
+      $('#scratchCard')[0].seleted = false;
+      ui.$btn.on('click','',function() {
+        $('#scratchCard').show();
+      });
+      ui.$signBtn.on('click','',function() {
+        var point = (+$(this).attr('point')) + (+$('#score').text());
+        self.sendAjax(uid,point);
+      });
+    },
+    sendAjax : function(uid,score) {
+      var self = this;
+      var options = {
+        'url' : '/updateScore',
+        'dataType' : 'json',
+        'type' : 'GET',
+        'data' : {uid : uid, score : score},
+        'success' : function(data) {
+          $('#score').text(score);
+          $('#scratchCard').hide();
+          ui.$btn.text('已签到').attr('disabled','disabled');
+          self.changeSignStatus();
+        },
+        'error' : function(err) {
+        }
+      }
+      $.ajax(options);
+    },
+    //改变签到状态
+    changeSignStatus : function() {
+      var options = {
+        'url' : '/changeSignStatus',
+        'dataType' : 'json',
+        'type' : 'POST',
+        'success' : function(data) {
+        },
+        'error' : function(err) {
+        }
+      }
+      $.ajax(options);
+    },
 
-		initJsChart : function() {
-			var options = {
-				'url' : '/getRanking',
-				'dataType' : 'json',
-				'type' : 'GET',
-				'success' : function(data) {
-					console.log(data)
-					var usersArr = [];
-					for(var i=0; i<3; i++) {
-						usersArr.push([data[i]['name'] || '--' ,data[i]['score']]);
-					}
+    initJsChart : function() {
+      var self = this;
+      var options = {
+        'url' : '/getRanking',
+        'dataType' : 'json',
+        'type' : 'GET',
+        'success' : function(data) {
+          self.renderRanking(data);
+        },
+        'error' : function(err) {
+        }
+      }
+      $.ajax(options);
+    },
 
-					var myData = new Array(usersArr[0], usersArr[1], usersArr[2]);
-					var colors = ['#FACC00', '#FB9900', '#FB6600'];
-					var myChart = new JSChart('graph', 'pie');
-					myChart.setDataArray(myData);
-					myChart.colorizePie(colors);
-					myChart.setTitle('前端社区积分前三排行榜');
-					myChart.setTitleColor('#857D7D');
-					myChart.setPieUnitsColor('#9B9B9B');
-					myChart.setPieValuesColor('#6A0000');
-					myChart.setSize(270, 300);
-					myChart.draw();
-					
-				},
-				'error' : function(err) {
-					console.log(err)
-				}
-			}
-			$.ajax(options);
-			
-		}
- 	}
+    renderRanking　 : function (data) {
+      var htmlStr = '';
+      for(var i=0; i<data.length; i++) {
+        if(data[i]['role'] == '2') {
+          // i--;
+        }else{
+          Page.pageData.user[data[i]['_id']] = data[i];
+          Page.pageData.user[data[i]['_id']]['sort'] = i+1;
+          Page.pageData.userSort[i+1] = data[i];  
+        }
+      }
 
- 	Page.init();
+      for(var i=0; i<data.length; i++) {
+        if(data[i]['role'] != '2') {
+
+          if(i === 0) {
+            htmlStr += '<tr>'
+                +'    <td><label class="label label-success">'+(i+1)+'</label></td><td>'+data[i]['name']+'</td><td>'+data[i]['score']+'</td>' 
+                +'</tr>';
+          } else if(i === 1) {
+            htmlStr += '<tr>'
+                +'    <td><label class="label label-warning">'+(i+1)+'</label></td><td>'+data[i]['name']+'</td><td>'+data[i]['score']+'</td>' 
+                +'</tr>';
+          } else if(i === 2) {
+            htmlStr += '<tr>'
+                +'    <td><label class="label label-info">'+(i+1)+'</label></td><td>'+data[i]['name']+'</td><td>'+data[i]['score']+'</td>' 
+                +'</tr>';
+          }
+        }
+      }
+
+      var curUser = Page.pageData.user[uid];
+      if(curUser) {
+        if(curUser['sort']==4) {
+          htmlStr += '<tr>'
+                +'    <td>4</td><td>'+curUser['name']+'</td><td>'+curUser['score']+'</td>' 
+                +'</tr>';
+        }else if(curUser['sort'] > 4){
+          htmlStr += '<tr>'
+                +'    <td>...</td><td></td><td></td>' 
+                +'</tr>';
+          htmlStr += '<tr>'
+                +'    <td>'+curUser['sort']+'</td><td>'+curUser['name']+'</td><td>'+curUser['score']+'</td>' 
+                +'</tr>';
+        }
+
+        if(curUser['sort'] != ( data.length - 1) ) {
+          htmlStr += '<tr>'
+                +'    <td>...</td><td></td><td></td>' 
+                +'</tr>';
+        }
+
+      }
+
+      ui.$rankContainer.html(htmlStr);
+    }
+
+
+  }
+
+  Page.init();
 });
 
